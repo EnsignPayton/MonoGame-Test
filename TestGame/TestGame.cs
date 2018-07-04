72b12eab9a99f452dbc7d3fca33d9f7ef3bc9c6c
Logan Payton <Logan.Payton@javs.com>
2018-07-04 11:25:48 -0400

    Format generated code and add readme

---
+++ TestGame/TestGame.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TestGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class TestGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public TestGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+        }
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
