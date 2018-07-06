28b1d70aa879e4837ca6938dc51775158235e9e4
Logan Payton <Logan.Payton@javs.com>
2018-07-05 21:56:41 -0400

    Use refs from TestGame

--- TestGame/Entities/Player.cs
+++ TestGame/Entities/Player.cs
@@ -8,9 +8,6 @@ namespace TestGame.Entities
 {
     public class Player : DrawableGameComponent
     {
-        private readonly InputState _inputState;
-        private readonly SpriteBatch _spriteBatch;
-
         private SoundEffect _fireEffect;
         private SoundEffectInstance _fireEffectInstance;
         private Texture2D _sprite;
@@ -18,11 +15,11 @@ namespace TestGame.Entities
         private Point _size;
         private Rectangle _bounds;
 
-        public Player(Game game, InputState inputState, SpriteBatch spriteBatch) : base(game)
+        public Player(Game game) : base(game)
         {
-            _inputState = inputState;
-            _spriteBatch = spriteBatch;
         }
+
+        public new TestGame Game => (TestGame) base.Game;
 
         protected override void LoadContent()
         {
@@ -39,44 +36,44 @@ namespace TestGame.Entities
         {
             float deltaTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
 
-            if (_inputState.KeyDown(Keys.A) || _inputState.KeyDown(Keys.Left))
+            if (Game.InputState.KeyDown(Keys.A) || Game.InputState.KeyDown(Keys.Left))
             {
                 _position.X -= deltaTime * 32.0f;
                 UpdateBounds();
             }
 
-            if (_inputState.KeyDown(Keys.D) || _inputState.KeyDown(Keys.Right))
+            if (Game.InputState.KeyDown(Keys.D) || Game.InputState.KeyDown(Keys.Right))
             {
                 _position.X += deltaTime * 32.0f;
                 UpdateBounds();
             }
 
-            if (_inputState.KeyDown(Keys.S) || _inputState.KeyDown(Keys.Down))
+            if (Game.InputState.KeyDown(Keys.S) || Game.InputState.KeyDown(Keys.Down))
             {
                 _position.Y += deltaTime * 32.0f;
                 UpdateBounds();
             }
 
-            if (_inputState.KeyDown(Keys.W) || _inputState.KeyDown(Keys.Up))
+            if (Game.InputState.KeyDown(Keys.W) || Game.InputState.KeyDown(Keys.Up))
             {
                 _position.Y -= deltaTime * 32.0f;
                 UpdateBounds();
             }
 
-            if (_inputState.KeyPressed(Keys.Space) ||
-                _inputState.MousePressed(MouseButton.LeftButton) ||
-                _inputState.MousePressed(MouseButton.RightButton))
+            if (Game.InputState.KeyPressed(Keys.Space) ||
+                Game.InputState.MousePressed(MouseButton.LeftButton) ||
+                Game.InputState.MousePressed(MouseButton.RightButton))
             {
                 _fireEffectInstance.Play();
             }
 
-            if (_inputState.KeyPressed(Keys.Q))
+            if (Game.InputState.KeyPressed(Keys.Q))
             {
                 _size = new Point(_size.X << 1, _size.Y << 1);
                 UpdateBounds();
             }
 
-            if (_inputState.KeyPressed(Keys.E))
+            if (Game.InputState.KeyPressed(Keys.E))
             {
                 _size = new Point(_size.X >> 1, _size.Y >> 1);
                 UpdateBounds();
@@ -87,7 +84,7 @@ namespace TestGame.Entities
 
         public override void Draw(GameTime gameTime)
         {
-            _spriteBatch.Draw(_sprite, _bounds, Color.White);
+            Game.SpriteBatch.Draw(_sprite, _bounds, Color.White);
 
             base.Draw(gameTime);
         }
